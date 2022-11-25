0b0cf21157133efde82039af5fad3e9ac12c222b
ROOZBEH <MR.FAKE@gmail.com>
2022-11-25 19:37:48 +0330

    make our CustomUser for project and our admin and after that we makemigrations

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+
+@admin.register(CustomUser)
+class CustomAdmin(UserAdmin):
+    model = CustomUser
+    add_form = CustomUserChangeForm
+    FORM = CustomUserChangeForm
+    list_display = ('email', 'username',)
+    
--- config/settings.py
+++ config/settings.py
@@ -136,3 +136,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# accounts CustomUser config 
+
+AUTH_USER_MODEL = 'accounts.CustomUser' 
