79ff92247d8aaea6602522e1f83c0a54c4530b13
ROOZBEH <MR.FAKE@gmail.com>
2022-12-08 19:43:06 +0330

    add our product in home view and change something in home page

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 
-from django.views.generic import TemplateView
+from django.views import generic
 
+from product.models import Product
 
-class HomePageView(TemplateView):
+class HomePageView(generic.ListView):
+    model = Product
     template_name = 'home.html'
+    context_object_name = 'products'
 
 
-class ContactUsPageView(TemplateView):
+class ContactUsPageView(generic.TemplateView):
     template_name = 'contact_us.html'
 
