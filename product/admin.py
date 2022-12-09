91e2a5d83b35f0b19fd6c9623f55526a9b6328ba
ROOZBEH <MR.FAKE@gmail.com>
2022-12-09 11:18:47 +0330

    add CommentInline class for product in admin and make templatetags for active_comments

--- product/admin.py
+++ product/admin.py
@@ -2,10 +2,19 @@ from django.contrib import admin
 
 from .models import Product, Comment
 
+
+class CommentsInline(admin.TabularInline):
+    model = Comment
+    list_display = ['author', 'body', 'star', 'active']
+    extra = 1
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'text', 'active']
 
+    inlines = [
+        CommentsInline,
+    ]
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
