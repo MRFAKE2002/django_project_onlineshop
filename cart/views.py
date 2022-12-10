ca5ff444859151d924d24da0b7153c002a8f39ca
ROOZBEH <MR.FAKE@gmail.com>
2022-12-10 19:21:16 +0330

    make add_product_to_cart view

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .forms import AddProductToCart
+from .cart import Cart
+from product.models import Product
 
 def cart_detail_view(request):
-    return render(request, 'cart/cart_detail.html')
+    cart = Cart(request)
+    
+    return render(request, 'cart/cart_detail.html', {'cart': cart})
+
+def add_product_to_cart(request, product_id):
+    cart = Cart(request)
+    
+    product = get_object_or_404(Product, id=product_id)
+    
+    cart_form = AddProductToCart(request.POST)
+    
+    if cart_form.is_valid():
+        cleaned_data = cart_form.cleaned_data
+        
+        quantity = cleaned_data['quantity']
+        
+        cart.add_to_cart(product, quantity)
+    
+    return redirect('cart:cart_detail')
